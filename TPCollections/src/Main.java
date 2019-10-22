eb086c151e6ac73c58b980815e11a6be24ea7a18
yoannBiheux <ybiheux@gmail.com>
2019-10-22 17:05:53 +0200

    push 6 22/10/2019

---
+++ TPCollections/src/Main.java
@@ -0,0 +1,81 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.StringTokenizer;
+import java.util.Map.Entry;
+
+public class Main {
+
+	public static void main(String[] args) 
+	{
+//		Map<Integer, String> stringIn = new HashMap<Integer, String>();
+//		stringIn.put(7,"Paris");
+//		stringIn.put(5,"Rome" ) ;
+//		stringIn.put (1,"Manchester") ;
+//		stringIn.put (3,"Barcelone") ;
+//		String chaineCaractereString ="Manchester ou Barcelone mais jamais Paris ni Rome";
+//		StringTokenizer st = new StringTokenizer(chaineCaractereString," ,.");
+//		String newString ="";
+//		
+//		while(st.hasMoreTokens())
+//		{
+//
+//			String sousChaineString = st.nextToken();
+//			
+//			if(stringIn.containsValue(sousChaineString))
+//			{
+//				for(Entry<Integer,String> e: stringIn.entrySet())
+//				{
+//					
+//					if (sousChaineString.equals(e.getValue().toString())) 
+//					{
+//						newString = newString + e.getKey().toString() + " ";
+//						break;
+//
+//					}
+//					
+//				}
+//			}
+//			else
+//			{
+//				newString = newString+ sousChaineString+" ";
+//			}
+//		}
+//		
+//		System.out.println(newString);
+		
+		Personne p1 = new Personne(1,"Biheux","yoann");
+		Personne p2 = new Personne(2,"Richard","nathalie");
+		Personne p3 = new Personne(3,"Davoust","fabrice");
+		Personne p4 = new Personne(4,"Canonne","philippe");
+		Personne p5 = new Personne(5,"Zinetti","victoire");
+		
+		Map<String, Personne> personnes = new HashMap();
+		personnes.put(p1.getNomPrenom(), p1);
+		personnes.put(p2.getNomPrenom(), p2);
+		personnes.put(p3.getNomPrenom(), p3);
+		personnes.put(p4.getNomPrenom(), p4);
+		personnes.put(p5.getNomPrenom(), p5);
+		
+		
+		
+		for(Entry<String,Personne> e: personnes.entrySet())
+		{
+			if(e.getValue().getNum()%2==0)
+				System.out.println(e.getKey());
+			else
+				System.out.println(e.getValue());
+	
+		}
+		
+		
+		
+		
+
+		
+
+
+	}
+
+}
---
+++ TPCollections/src/Personne.java
@@ -0,0 +1,51 @@
+
+public class Personne 
+{
+	
+	private int num;
+	private String nom;
+	private String prenom;
+	
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getPrenom() {
+		return prenom;
+	}
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	public Personne(int num, String nom, String prenom) 
+	{
+
+		this.num = num;
+		this.nom = nom;
+		this.prenom = prenom;
+	}
+	public Personne() 
+	{
+
+
+	}
+	
+	public String getNomPrenom()
+	{
+		return this.nom+" "+this.prenom;
+	}
+	
+	
+	
+	
+	
+	
+
+}
